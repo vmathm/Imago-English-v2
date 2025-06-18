99fc0024daf26215950344eab75fda853a849a27
Vitor Moraes <vitor-mathias@hotmail.com>
2025-06-18 14:39:02 -0300

    adds LoginManager()

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,44 @@
 from flask import Flask
+from dotenv import load_dotenv
+from flask_login import LoginManager
+import os
+
+from config import Config
+from .models.base import Base
+from .database import init_engine
+
+login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile('../config.py')  
+    # Load environment variables from .env file
+    load_dotenv()
 
-    # Register Blueprints
+    app = Flask(__name__)
+    login_manager.init_app(app)
+
+    # Dynamically load config based on FLASK_CONFIG env variable
+    app.config.from_object("config.Config")
+    print("flask_config:", os.getenv("FLASK_CONFIG"))
+    print("DEBUG mode:", app.config["DEBUG"])
+    print("DEBUG:", app.debug)
+    
+   # Initialize DB
+    engine, db_session = init_engine(app)
+    Base.metadata.create_all(engine)
+
+
+    
+
+    # Register blueprints
     from .auth.routes import bp as auth_bp
     app.register_blueprint(auth_bp)
+    from .dashboard.routes import bp as dashboard_bp
+    app.register_blueprint(dashboard_bp)
+
     '''
     from .flashcards.routes import bp as flashcards_bp
     from .progress.routes import bp as progress_bp
-    from .dashboard.routes import bp as dashboard_bp
+   
     from .audiobook.routes import bp as audiobook_bp
 
     
@@ -19,4 +47,5 @@ def create_app():
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(audiobook_bp)
     '''
-    return app
+
+    return app  
