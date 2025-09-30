335b0d2758a8b25ca70cab9a0d677e5c09033d27
Vitor Moraes <vitor-mathias@hotmail.com>
2025-09-30 15:58:17 -0300

    home fix: if ALLOW_SEEDED_USERS correctly logs in a user with ID on the URL. Current users (if any) are logged o ut first.

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, redirect, url_for, current_app
+from flask import Blueprint, abort, redirect, url_for, current_app
 from flask_login import current_user
+from app.models import User
 
 bp = Blueprint("home", __name__)
 
@@ -9,10 +10,15 @@ bp = Blueprint("home", __name__)
 @bp.route("/<user_id>")
 def index(user_id):
 
-    if current_user.is_authenticated:
-        return redirect(url_for("dashboard.index"))
+    if user_id is None:
+        if current_user.is_authenticated:
+            return redirect(url_for("dashboard.index"))
     
-    if current_app.config["ALLOW_SEEDED_USERS"]:
-        return redirect(url_for("auth.demo_login", user_id=user_id))
+        if current_app.config["ALLOW_SEEDED_USERS"]:
+            return redirect(url_for("auth.demo_login"))
     else:
-        return redirect(url_for("dashboard.index"))
+        if not current_app.config["ALLOW_SEEDED_USERS"]:
+            abort(403)
+        return redirect(url_for("auth.demo_login", user_id=user_id))
+
+        
