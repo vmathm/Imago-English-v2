65f97d71e0fdc17435c72bbf0846ee489c1ac43b
Vitor Moraes <vitor-mathias@hotmail.com>
2025-10-02 16:18:08 -0300

    adds a dummy option to level select menu in teacher dashboard.

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -28,7 +28,7 @@ class ChangeStudentLevelForm(FlaskForm):
     student_id = HiddenField(validators=[DataRequired()])
     level = SelectField(
         'Level',
-        choices=[('A1','A1'), ('A2','A2'), ('B1','B1'), ('B2','B2'), ('C1','C1'), ('C2','C2')],
+        choices=[('', 'Level'),('A1','A1'), ('A2','A2'), ('B1','B1'), ('B2','B2'), ('C1','C1'), ('C2','C2')],
         validators=[DataRequired()]
     )
     submit = SubmitField('Update level')
