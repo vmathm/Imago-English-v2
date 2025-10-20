eeb83a442ff9a92f49759542139831eed0dd4c9a
Vitor Moraes <vitor-mathias@hotmail.com>
2025-10-20 16:41:23 -0300

    fixes main route '/' not working properly when ALLOW_SEEDED_USERS=False

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, abort, redirect, url_for, current_app
-from flask_login import current_user
+from flask_login import current_user, logout_user
 from app.models import User
 
 bp = Blueprint("home", __name__)
@@ -10,12 +10,14 @@ bp = Blueprint("home", __name__)
 @bp.route("/<user_id>")
 def index(user_id):
 
+    if current_user.is_authenticated:
+        return redirect(url_for("dashboard.index"))
+
     if user_id is None:
-        if current_user.is_authenticated:
-            return redirect(url_for("dashboard.index"))
-    
         if current_app.config["ALLOW_SEEDED_USERS"]:
             return redirect(url_for("auth.demo_login"))
+        
+        return redirect(url_for("dashboard.index"))
     else:
         if not current_app.config["ALLOW_SEEDED_USERS"]:
             abort(403)
