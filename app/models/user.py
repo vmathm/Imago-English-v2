f20729edf9bfad8e465bcf08ce8c7b1ded6aa000
vmathm <80046997+vmathm@users.noreply.github.com>
2026-03-26 09:55:57 -0300

    adds billing_mode= internal or external to users

--- app/models/user.py
+++ app/models/user.py
@@ -26,6 +26,7 @@ class User(UserMixin, Base):
     user_stripe_id = Column(String(50), nullable=True)
     join_date = Column(Date, nullable=True)
     last_payment_date = Column(Date, nullable=True)
+    billing_mode = Column(String(20), nullable=False, default="external")
 
     active = Column(Boolean, nullable=False, default=False)  # replaces is_active
     study_streak = Column(Integer, nullable=True, default=0)
